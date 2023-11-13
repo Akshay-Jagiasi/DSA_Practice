171028d9a796818466de22ff9439240a62d4a124
Akshay_Jagiasi <akshayjagiasi1804@gmail.com>
2023-11-13 12:17:53 +0530

    -

--- 4___nov_6_TO_nov_12.java
+++ 4___nov_6_TO_nov_12.java
@@ -430,3 +430,86 @@ class Solution {
         return count;
     }
 }
+
+
+
+//------------------------------------------------------------------------------------
+
+//𝐐𝐮𝐞𝐬𝐭𝐢𝐨𝐧 10
+// Minimum Cost to Move Chips to The Same Position
+// We have n chips, where the position of the ith chip is position[i].
+// We need to move all the chips to the same position. In one step, we can change the position of the ith chip from position[i] to:
+// position[i] + 2 or position[i] - 2 with cost = 0.
+// position[i] + 1 or position[i] - 1 with cost = 1.
+// Return the minimum cost needed to move all the chips to the same position.
+
+// Example 1:
+// Input: position = [1,2,3]
+// Output: 1
+// Explanation: First step: Move the chip at position 3 to position 1 with cost = 0.
+// Second step: Move the chip at position 2 to position 1 with cost = 1.
+// Total cost is 1.
+
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int evenCount = 0;
+        int oddCount = 0;
+
+        for(int chip : position){
+            if(chip %2 == 0){
+                evenCount++;
+            }else{
+                oddCount++;
+            }
+        }
+        return Math.min(evenCount, oddCount);
+    }
+}
+
+
+//------------------------------------------------------------------------------------
+
+//𝐐𝐮𝐞𝐬𝐭𝐢𝐨𝐧 11
+// Cells with Odd Values in a Matrix
+// There is an m x n matrix that is initialized to all 0's. There is also a 2D array indices where each indices[i] = [ri, ci] represents a 0-indexed location to perform some increment operations on the matrix.
+// For each location indices[i], do both of the following:
+// Increment all the cells on row ri.
+// Increment all the cells on column ci.
+// Given m, n, and indices, return the number of odd-valued cells in the matrix after applying the increment to all locations in indices.
+
+// Example 1:
+// Input: m = 2, n = 3, indices = [[0,1],[1,1]]
+// Output: 6
+// Explanation: Initial matrix = [[0,0,0],[0,0,0]].
+// After applying first increment it becomes [[1,2,1],[0,1,0]].
+// The final matrix is [[1,3,1],[1,3,1]], which contains 6 odd numbers.
+
+class Solution {
+    public int oddCells(int m, int n, int[][] indices) {
+        int[][] matrix = new int[m][n];
+        // Increment cells based on indices
+        for (int[] index : indices) {
+            int row = index[0];
+            int col = index[1];
+            // Increment cells in the entire row
+            for (int i = 0; i < n; i++) {
+                matrix[row][i]++;
+            }
+            // Increment cells in the entire column
+            for (int i = 0; i < m; i++) {
+                matrix[i][col]++;
+            }
+        }
+        // Count odd-valued cells
+        int oddCount = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] % 2 == 1) {
+                    oddCount++;
+                }
+            }
+        }
+
+        return oddCount;
+    }
+}
