4db727d83ca4697fb432677563485295f917194f
Akshay_Jagiasi <akshayjagiasi1804@gmail.com>
2024-01-11 19:17:10 +0530

    -

--- 12__jan_8_TO_jan_14.java
+++ 12__jan_8_TO_jan_14.java
@@ -174,3 +174,193 @@ public class AVL {
         System.out.println(tree.height());
     }
 }
+
+
+
+//____________________________________________________________________________________________________________________
+// Problem Statement:
+// Implement a Segment Tree data structure to efficiently handle two operations on an array:
+
+// Query Operation:
+// Given indices qsi and qei, find the sum of elements in the range [qsi, qei] of the original array.
+
+// Update Operation:
+// Given an index index and a new value value, update the element at the specified index in the original array. Ensure the segment tree is updated accordingly.
+
+package tree;
+
+public class SegmentTree {
+    private static class Node{
+        int data;
+        int startInterval;
+        int endInterval;
+        Node left;
+        Node right;
+
+        public Node(int startInterval, int endInterval){
+            this.startInterval = startInterval;
+            this.endInterval = endInterval;
+        }
+    }
+
+    Node root;
+
+    public SegmentTree(int[] arr){
+        //create a tree using this array
+        this.root = constructTree(arr, 0, arr.length-1);
+    }
+
+    public Node constructTree(int[] arr, int start, int end){
+        if(start == end){
+            Node leaf = new Node(start, end);
+            leaf.data = arr[start];
+            return leaf;
+        }
+
+        //create new node with index you are at 
+        Node node = new Node(start, end);
+        int mid = (start+end)/2;
+
+        node.left = this.constructTree(arr, start, mid);
+        node.right = this.constructTree(arr, mid+1, end);
+
+        node.data = node.left.data + node.right.data;
+        return node;
+    }
+
+    public void display(){
+        display(this.root);
+    }
+
+    private void display(Node node){
+        String str = "";
+
+        if(node.left != null){
+            str = str + "Interval=[" + node.left.startInterval + "-" + node.left.endInterval + "] and data: " + node.left.data + " => ";
+        }else{
+            str = str + "No left child";
+        }
+
+        //for current node 
+        str = str + "Interval=[" + node.startInterval + "-" + node.endInterval + "] and data: " + node.data + " <= ";
+
+        if(node.right != null){
+            str = str + "Interval=[" + node.right.startInterval + "-" + node.right.endInterval + "] and data: " + node.right.data;
+        }else{
+            str = str + "No right child";
+        }
+
+        System.out.println(str + "\n");
+
+        //call recursion
+        if(node.left != null){
+            display(node.left);
+        }
+
+        if(node.right != null){
+            display(node.right);
+        }
+    }
+
+    //query
+    public int query(int qsi, int qei){
+        return this.query(this.root, qsi, qei);
+    }
+
+    private int query(Node node, int qsi, int qei){
+        if(node.startInterval >= qsi && node.endInterval <= qei){
+            //node is completely lying inside query
+            return node.data;
+        }else if(node.startInterval > qei || node.endInterval < qsi){
+            //completely outside
+            return 0;
+        }else{
+            // partial overlap with the query, recursion on both left and right
+            return this.query(node.left, qsi, qei) + this.query(node.right, qsi, qei);
+        }
+    }
+
+    //update
+    public void update(int index, int value){
+        this.root.data = update(this.root, index, value);
+    }
+    
+    private int update(Node node, int index, int value){
+        if(index >= node.startInterval && index <= node.endInterval){
+            if(index == node.startInterval && index == node.endInterval){
+                node.data = value;
+                return node.data;
+            }else{
+                int leftAns = update(node.left, index, value);
+                int rightAns = update(node.right, index, value);
+
+                node.data = leftAns + rightAns;
+                return node.data;
+            }
+        } 
+
+        return node.data;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 8, 6, 7, -2, -8, 4, 9};
+        SegmentTree segmentTree = new SegmentTree(arr);
+        int qsi = 1;
+        int qei = 6;
+        System.out.println("Segment Tree Structure:\n");
+        segmentTree.display();
+        System.out.println("The sum of the range from "+qsi+" to "+qei+" => "+segmentTree.query(qsi,qei) +"\n");
+        segmentTree.update(3,100);
+        System.out.println("___________________________________________________________________________________________________");
+        System.out.println("Segment Tree Structure after the UPDATE:\n");
+        segmentTree.display();
+        System.out.println("The sum of the range from "+qsi+" to "+qei+" after the update => "+segmentTree.query(qsi,qei));
+    }
+}
+
+
+//OUTPUT: 
+// Segment Tree Structure:
+
+// Interval=[0-3] and data: 24 => Interval=[0-7] and data: 27 <= Interval=[4-7] and data: 3
+// Interval=[0-1] and data: 11 => Interval=[0-3] and data: 24 <= Interval=[2-3] and data: 13
+// Interval=[0-0] and data: 3 => Interval=[0-1] and data: 11 <= Interval=[1-1] and data: 8
+// No left childInterval=[0-0] and data: 3 <= No right child
+// No left childInterval=[1-1] and data: 8 <= No right child
+// Interval=[2-2] and data: 6 => Interval=[2-3] and data: 13 <= Interval=[3-3] and data: 7
+// No left childInterval=[2-2] and data: 6 <= No right child
+// No left childInterval=[3-3] and data: 7 <= No right child
+// Interval=[4-5] and data: -10 => Interval=[4-7] and data: 3 <= Interval=[6-7] and data: 13
+// Interval=[4-4] and data: -2 => Interval=[4-5] and data: -10 <= Interval=[5-5] and data: -8
+// No left childInterval=[4-4] and data: -2 <= No right child
+// No left childInterval=[5-5] and data: -8 <= No right child
+// Interval=[6-6] and data: 4 => Interval=[6-7] and data: 13 <= Interval=[7-7] and data: 9
+// No left childInterval=[6-6] and data: 4 <= No right child
+// No left childInterval=[7-7] and data: 9 <= No right child
+
+// The sum of the range from 1 to 6 => 15
+
+// ___________________________________________________________________________________________________
+// Segment Tree Structure after the UPDATE:
+
+// Interval=[0-3] and data: 117 => Interval=[0-7] and data: 120 <= Interval=[4-7] and data: 3
+// Interval=[0-1] and data: 11 => Interval=[0-3] and data: 117 <= Interval=[2-3] and data: 106
+// Interval=[0-0] and data: 3 => Interval=[0-1] and data: 11 <= Interval=[1-1] and data: 8
+// No left childInterval=[0-0] and data: 3 <= No right child
+// No left childInterval=[1-1] and data: 8 <= No right child
+// Interval=[2-2] and data: 6 => Interval=[2-3] and data: 106 <= Interval=[3-3] and data: 100
+// No left childInterval=[2-2] and data: 6 <= No right child
+// No left childInterval=[3-3] and data: 100 <= No right child
+// Interval=[4-5] and data: -10 => Interval=[4-7] and data: 3 <= Interval=[6-7] and data: 13
+// Interval=[4-4] and data: -2 => Interval=[4-5] and data: -10 <= Interval=[5-5] and data: -8
+// No left childInterval=[4-4] and data: -2 <= No right child
+// No left childInterval=[5-5] and data: -8 <= No right child
+// Interval=[6-6] and data: 4 => Interval=[6-7] and data: 13 <= Interval=[7-7] and data: 9
+// No left childInterval=[6-6] and data: 4 <= No right child
+// No left childInterval=[7-7] and data: 9 <= No right child
+
+// The sum of the range from 1 to 6 after the update => 108
+
+
+
+//____________________________________________________________________________________________________________________
