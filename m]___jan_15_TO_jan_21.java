6617cb66100cbb535f602824f1bafd5c5bce7a90
Akshay_Jagiasi <akshayjagiasi1804@gmail.com>
2024-01-22 02:25:52 +0530

    -

--- m]___jan_15_TO_jan_21.java
+++ m]___jan_15_TO_jan_21.java
@@ -389,6 +389,49 @@ class Solution {
 
 
 //_______________________________________________________________________________________________________________
+https://leetcode.com/problems/diameter-of-binary-tree/
+
+// Runtime 0ms
+// Beats 100.00% of users with Java
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int diameter = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return diameter-1;
+    }
+
+    public int height(TreeNode node){
+        if(node == null){
+            return 0;
+        }
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+
+        int dia = leftHeight + rightHeight + 1;
+        diameter = Math.max(diameter, dia);
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
+
+
+//_______________________________________________________________________________________________________________
 // Problem Statement: Custom HashMap Implementations
 
 // Objective:
