dd86f932333f229bced62f751f30dc1d872ca008
Akshay_Jagiasi <akshayjagiasi1804@gmail.com>
2024-01-28 21:06:01 +0530

    -

--- n]___jan_22_TO_jan_28.java
+++ n]___jan_22_TO_jan_28.java
@@ -1039,6 +1039,85 @@ class Solution {
 
 
 //__________________________________________________________________________________________________________________________________________
+Q22: https://leetcode.com/problems/reorder-list/description/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        ListNode mid = middleNode(head);
+        // hs => head second
+        ListNode hs = reverseList(mid);
+
+        //hf => head first
+        ListNode hf = head;
+
+        //rearrange for the final output
+        while(hf != null && hs != null){
+            ListNode temp = hf.next;
+            hf.next = hs;
+            hf = temp;
+
+            temp = hs.next;
+            hs.next = hf;
+            hs = temp;
+        }
+
+        // next of tail to null
+        if (hf != null) {
+            hf.next = null;
+        }    
+    }
+
+    public ListNode middleNode(ListNode head) {
+      ListNode s = head;
+      ListNode f = head;
+
+      while (f != null && f.next != null) {
+          s = s.next;
+          f = f.next.next;
+      }
+      return s;
+    }
+    
+    public ListNode reverseList(ListNode head) {
+      if (head == null) {
+          return head;
+      }
+      ListNode prev = null;
+      ListNode present = head;
+      ListNode next = present.next;
+
+      while (present != null) {
+          present.next = prev;
+          prev = present;
+          present = next;
+          if (next != null) {
+              next = next.next;
+          }
+      }
+      return prev;
+    }
+
+
+    
+}
+
+
+
+//__________________________________________________________________________________________________________________________________________
 Q21: https://leetcode.com/problems/largest-rectangle-in-histogram/description/
 
 //Time Complexity  O(n^2)
