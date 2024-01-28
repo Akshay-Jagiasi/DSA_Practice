f4522d716003e588e6e1522d9ec0db8fe668b899
Akshay_Jagiasi <akshayjagiasi1804@gmail.com>
2024-01-28 19:33:58 +0530

    -

--- n]___jan_22_TO_jan_28.java
+++ n]___jan_22_TO_jan_28.java
@@ -867,3 +867,171 @@ void postOrderDFSStack(Node node) {
         System.out.print(stack2.pop().val + " ");
     }
 }
+
+
+
+//__________________________________________________________________________________________________________________________________________
+Q18: https://leetcode.com/problems/reverse-linked-list/description/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+ Runtime 0ms
+ Beats 100.00% of users with Java
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+       if (head == null) {
+           return head; 
+       }
+       ListNode prev = null;
+       ListNode present = head;
+       ListNode next = present.next;
+
+       while (present != null) {
+           present.next = prev;
+           prev = present;
+           present = next;
+           if (next != null) {
+               next = next.next;
+           }
+       }
+       return prev;
+   }
+}
+
+
+
+//__________________________________________________________________________________________________________________________________________
+Q19: https://leetcode.com/problems/reverse-linked-list-ii/description/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+ Runtime 0ms
+ Beats 100.00% of users with Java
+
+ //for future draw the linkedlist and debug the code for the understanding
+ class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (left == right) {
+            return head;
+        }
+
+        // skip the first left-1 nodes
+        ListNode current = head;
+        ListNode prev = null;
+        for (int i = 0; current != null && i < left - 1; i++) {
+            prev = current;
+            current = current.next;
+        }
+
+        ListNode last = prev;
+        ListNode newEnd = current;
+
+        // reverse between left and right
+        ListNode next = current.next;
+        for (int i = 0; current != null && i < right - left + 1; i++) {
+            current.next = prev;
+            prev = current;
+            current = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+
+        if (last != null) {
+            last.next = prev;
+        } else {
+            head = prev;
+        }
+
+        newEnd.next = current;
+        return head;
+    }
+}
+
+
+
+//__________________________________________________________________________________________________________________________________________
+Q20: https://leetcode.com/problems/palindrome-linked-list/description/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+Runtime 6ms
+Beats 53.63% of users with Java
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+      ListNode mid = middleNode(head);
+      ListNode headSecond = reverseList(mid);
+      ListNode rereverseHead = headSecond;
+
+      // compare both the halves
+      while (head != null && headSecond != null) {
+          if (head.val != headSecond.val) {
+              break;
+          }
+          head = head.next;
+          headSecond = headSecond.next;
+      }
+      reverseList(rereverseHead);
+
+      return head == null || headSecond == null;
+  }
+
+  public ListNode middleNode(ListNode head) {
+      ListNode s = head;
+      ListNode f = head;
+
+      while (f != null && f.next != null) {
+          s = s.next;
+          f = f.next.next;
+      }
+      return s;
+  }
+
+   public ListNode reverseList(ListNode head) {
+      if (head == null) {
+          return head;
+      }
+      ListNode prev = null;
+      ListNode present = head;
+      ListNode next = present.next;
+
+      while (present != null) {
+          present.next = prev;
+          prev = present;
+          present = next;
+          if (next != null) {
+              next = next.next;
+          }
+      }
+      return prev;
+  }
+}
