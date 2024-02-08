ea98505d3fffc045c4870692c5928553b81d9fa3
Akshay_Jagiasi <akshayjagiasi1804@gmail.com>
2024-02-08 15:17:29 +0530

    -

--- p]___feb_5_TO_feb_11.java
+++ p]___feb_5_TO_feb_11.java
@@ -105,3 +105,68 @@ class Solution {
         return null;
     }
 }
+
+
+
+//_______________________________________________________________________________________________________________________
+Q: https://leetcode.com/problems/check-if-n-and-its-double-exist/description/
+
+//Time Complexity: O(nlogn)
+//Runtime 3ms
+//Beats 38.71% of users with Java
+// class Solution {
+//     public boolean checkIfExist(int[] arr) {
+//         int n = arr.length;
+//         int zeroCount = 0;
+//         Arrays.sort(arr);
+//         for(int i=0; i<n; i++){
+//             int target = arr[i] * 2;
+//             if (arr[i] == 0) {
+//                 zeroCount++;
+//                 if (zeroCount >= 2) {
+//                     return true;
+//                 }
+//             }
+//             if(target != 0 && binarySearch(arr, 0, n-1, target)){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+//     boolean binarySearch(int[] arr, int start, int end, int target){
+//         while(start<=end){
+//             int mid = start+(end-start)/2;
+//             if(target == arr[mid]){
+//                 return true;
+//             }else if(target<arr[mid]){
+//                 end = mid-1;
+//             }else{
+//                 start=mid+1;
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+
+//TimeComplexity: O(n)
+// Runtime 2ms
+// Beats 91.58% of users with Java
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        boolean hasZero = false;
+        for(int num : arr){
+            if(num == 0){
+                if(hasZero) return true;
+                hasZero = true;
+            }else{
+                if(set.contains(num * 2) || (num % 2 == 0 && set.contains(num / 2))){
+                    return true;
+                }
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
