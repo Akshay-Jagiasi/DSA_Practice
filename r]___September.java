0806bab524bda6de9e05e05b0661f94cb2a2d2ee
Akshay_Jagiasi <akshayjagiasi1804@gmail.com>
2024-09-27 01:19:14 +0530

    -

--- r]___September.java
+++ r]___September.java
@@ -3992,3 +3992,39 @@ class Solution {
         return String.join(" ", words);
     }
 }
+
+
+//___________________________________________________________________________________________________________________________
+Q62: https://leetcode.com/problems/guess-number-higher-or-lower/?envType=study-plan-v2&envId=leetcode-75
+
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+ public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+
+        int start = 1;
+        int end = n;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            int result = guess(mid); // Call the guess API with mid
+
+            if (result == 0) {
+                return mid; 
+            } else if (result == 1) {
+                start = mid + 1; 
+            } else { 
+                end = mid - 1; 
+            }
+        }
+        return -1;
+    }
+}
